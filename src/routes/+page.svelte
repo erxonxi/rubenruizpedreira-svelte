afdf6fbecf75240f28be41868128af2992bc5f97
ruben.ruiz <rubenruizpedreira@gmail.com>
2023-05-14 23:09:35 +0200

    :lipstick: better content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,63 +3,61 @@
 
 <section>
 	<h1 id="ruben-ruiz-pedreira">Rubén Ruiz Pedreira</h1>
+	<h2 id="web-software-developer-freelancer">Web Software Developer &amp; Freelancer</h2>
 
-	<h2 id="passionate-web-software-developer-freelancer">
-		Passionate Web Software Developer &amp; Freelancer
-	</h2>
-
-	<h3 id="who-i-am">Who I Am</h3>
+	<h3 id="about-me">About Me</h3>
 
 	<p>
-		Hello! I&#39;m Rubén Ruiz Pedreira, a Web Software Developer with a passion for technology and a
-		constant desire to learn. I currently work at Netex Learning, where I implement new features,
-		improve existing code, and fix bugs, all while adhering to the principles of Test Driven Design
-		and best programming practices. In my spare time, I love working on small projects to explore
-		new technologies and bring my innovative ideas to life.
+		Hi, I'm Rubén Ruiz Pedreira, a dedicated Web Software Developer with a deep interest in
+		technology and a perpetual thirst for knowledge. I'm currently part of the team at Netex
+		Learning, where I enhance the user experience by developing new features, refining existing
+		code, and troubleshooting issues. I'm a firm believer in Domain Driven Design and always strive
+		to follow best programming practices. When I'm not at work, I enjoy experimenting with new
+		technologies and turning my creative ideas into reality through personal projects.
 	</p>
 
-	<h3 id="my-journey">My Journey</h3>
+	<h3 id="my-career-path">My Career Path</h3>
 
 	<p>
-		My journey in the tech world began at Nemon Intelligence in Business, where I started as a
-		Junior Web Software Developer and later transitioned into Quality Management. Currently, I&#39;m
-		a part of the team at Netex Learning, and I also undertake freelance projects that allow me to
-		apply my skills in diverse contexts.
+		I embarked on my tech journey at Nemon Intelligence in Business, where I cut my teeth as a
+		Junior Web Software Developer before moving into Quality Management. Now, I'm honing my skills
+		at Netex Learning and also taking on freelance projects that allow me to explore different
+		applications of my expertise.
 	</p>
 
-	<h3 id="what-i-do">What I Do</h3>
+	<h3 id="my-work">My Work</h3>
 
 	<p>
-		As a developer, I&#39;ve had the opportunity to work on a variety of projects. Some of my
-		notable freelance work includes a Wallet App for managing cryptocurrency wallets and a CLI
-		DevOps application for managing deployed services. At Netex Learning, I&#39;ve contributed to
-		the development of SmartED, a platform for managing school tasks and tracking student progress.
+		My career has given me the chance to work on a diverse array of projects. Some of my standout
+		freelance contributions include a Wallet App for managing cryptocurrency portfolios and a CLI
+		DevOps tool for overseeing deployed services. At Netex Learning, I've played a key role in
+		developing SmartED, a comprehensive platform for managing academic tasks and monitoring student
+		performance.
 	</p>
 
-	<h3 id="my-passions">My Passions</h3>
+	<h3 id="interests">Interests</h3>
 
 	<p>
-		Beyond development, I&#39;m fascinated by architecture and systems. Thanks to advancements like
-		Docker and Kubernetes, these areas have become more accessible to developers. When I&#39;m not
-		coding, you&#39;ll likely find me immersed in electronic music, my other great passion.
+		Apart from coding, I have a keen interest in architecture and systems, and I'm always excited to
+		explore new advancements like Docker and Kubernetes. When I'm not immersed in the tech world,
+		you'll probably find me enjoying electronic music, another of my passions.
 	</p>
 
-	<h3 id="my-skills">My Skills</h3>
+	<h3 id="expertise">Expertise</h3>
 
 	<p>
-		Over the years, I&#39;ve gained proficiency in a range of programming languages and tools.
-		I&#39;m skilled in ReactJS, Electron, TypeScript, and Golang, among others. I&#39;m also a
-		strong advocate for quality assurance and have experience incorporating end-to-end tests with
-		Cypress into the development process.
+		I've acquired a broad skill set over the years, mastering various programming languages and
+		tools. My expertise includes ReactJS, Electron, TypeScript, and Golang, to name a few. I'm also
+		a strong proponent of quality assurance and have integrated end-to-end testing with Cypress into
+		my development workflow.
 	</p>
 
-	<h3 id="let-s-connect">Let&#39;s Connect</h3>
+	<h3 id="connect-with-me">Connect with Me</h3>
 
 	<p>
-		I&#39;m always open to new opportunities and collaborations. Feel free to reach out to me on <a
-			href="https://www.linkedin.com/in/rub%C3%A9n-ruiz-pedreira/">LinkedIn</a
-		>
-		or explore my projects on <a href="https://github.com/your-github-link">GitHub</a>.
+		I'm always eager to explore new opportunities and collaborations. Don't hesitate to connect with
+		me on <a href="https://www.linkedin.com/in/rub%C3%A9n-ruiz-pedreira/">LinkedIn</a>
+		or check out my work on <a href="https://github.com/your-github-link">GitHub</a>.
 	</p>
 </section>
 
@@ -67,12 +65,48 @@
 	section {
 		display: flex;
 		flex-direction: column;
-		justify-content: center;
 		flex: 0.6;
-		padding: 0 1em;
+		padding: 0 2.5em;
+		padding-bottom: 2rem;
+		line-height: 1.6;
 	}
 
 	h1 {
-		width: 100%;
+		font-family: var(--font-mono);
+		font-size: 2.5em;
+		color: #333;
+		margin-bottom: 0.25em;
+	}
+
+	h2 {
+		font-family: var(--font-mono);
+		font-size: 1.8em;
+		color: #666;
+		margin-bottom: 0.5em;
+	}
+
+	h3 {
+		font-family: var(--font-mono);
+		font-size: 1.75em;
+		color: #333;
+		margin-bottom: 0.5em;
+		margin-left: 1em;
+	}
+
+	p {
+		font-size: 1.1em;
+		color: #666;
+		text-align: justify;
+		line-height: 1.8;
+	}
+
+	a {
+		font-family: var(--font-mono);
+		color: #007bff;
+		text-decoration: none;
+	}
+
+	a:hover {
+		color: #0056b3;
 	}
 </style>
